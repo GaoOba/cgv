f4e13b07bdee124a8f7542eb44501a5f4bc0eb07
root <gaoleioba@126.com>
2019-03-08 21:17:10 +0800

    第一次上传 springboot框架搭建

---
+++ src/main/java/com/qianfeng/cgv/CgvApplication.java
@@ -0,0 +1,15 @@
+package com.qianfeng.cgv;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.qianfeng.cgv.dao")
+public class CgvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgvApplication.class, args);
+    }
+
+}
